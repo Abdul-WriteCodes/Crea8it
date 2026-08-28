0c533af37509a0c9e9b12dce03ff3c74ce1f2a34
Abdul <145736001+Abdul-WriteCodes@users.noreply.github.com>
2026-08-28 21:43:51 +0100

    Modify Crea8it branding in register page
    Updated the Crea8it branding in the hero section.

--- pages/register.py
+++ pages/register.py
@@ -397,7 +397,7 @@ def show():
     st.markdown("""
     <div class="lp-hero">
       <div class="lp-wordmark">
-        <span style="color:#FFD700;">Crea8it</span><span style="color:#00B4D8;">Lab</span>
+        <span style="color:#FFD700;">Crea8it </span><br><span style="color:#00B4D8;">Lab</span>
       </div>
       <div class="lp-tagline">
         Build ⚙️ &nbsp;●&nbsp; Launch 🚀 &nbsp;●&nbsp; Learn 🤸🏻 &nbsp;●&nbsp; Win 🏆
