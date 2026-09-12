fa519955a3e14c97b6f3a80cb4073780de50f303
Abdul <145736001+Abdul-WriteCodes@users.noreply.github.com>
2026-09-12 02:52:42 +0100

    Update sidebar state and link button text

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ st.set_page_config(
     page_title="Crea8it Labs",
     page_icon="🧩",
     layout="centered",
+    initial_sidebar_state="expanded",
 )
 
 from utils.theme import apply_css
@@ -40,7 +41,7 @@ if st.session_state.get("logged_out_redirect_url"):
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         st.link_button(
-            f"Return to {url.replace('https://', '')}",
+            "Return to Crea8it",
             url,
             type="primary",
             width="stretch",
