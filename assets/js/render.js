f6429c7ea43e47ecb3972aad423cc79292ee5f22
Abdul <145736001+Abdul-WriteCodes@users.noreply.github.com>
2026-09-19 03:18:28 +0100

    Refactor footer links structure in render.js

--- assets/js/render.js
+++ assets/js/render.js
@@ -22,11 +22,10 @@
 
     // [heading, [[href, label, opensNewTab]]]
     var FOOTER = [
-        ["Useful Links",      [["index.html","Home"],["resources.html","Resources"],["programs.html","Programs"],["projects.html","Products"],["impact.html","Impacts"]]],
-        ["For Organizations", [["run-a-cohort.html","Run Your Own Cohort"],["organisation-terms.html","Organisation Terms"]]],
-        ["Community",         [[COMMUNITY,"Join on WhatsApp",true]]],
-        ["The Lab",           [[LAB_URL,"Enter Lab"]]],
-        ["Legal & Help",      [["faqs.html","FAQs"],["terms.html","Terms of Service"],["privacy.html","Privacy & Cookies"]]]
+        ["Explore",       [["index.html","Home"],["programs.html","Programs"],["resources.html","Resources"],["projects.html","Products"],["impact.html","Impacts"]]],
+        ["Organizations", [["run-a-cohort.html","Run a Cohort"],["organisation-terms.html","Organisation Terms"]]],
+        ["Connect",       [[LAB_URL,"Enter Lab"],[COMMUNITY,"Join on WhatsApp",true]]],
+        ["Legal & Help",  [["faqs.html","FAQs"],["terms.html","Terms of Service"],["privacy.html","Privacy & Cookies"]]]
     ];
 
     var SOCIALS = `<div class="footer-socials"><!-- Twitter / X --><a href="https://twitter.com/abdul_git07" target="_blank" rel="noopener" aria-label="Twitter / X"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.746l7.73-8.835L1.254 2.25H8.08l4.262 5.636 5.901-5.636Zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg></a><!-- LinkedIn --><a href="https://linkedin.com/in/anafiabdul" target="_blank" rel="noopener" aria-label="LinkedIn"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg></a><!-- Blogger --><a href="https://abdulbuilds.blogspot.com" target="_blank" rel="noopener" aria-label="Blog"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.976 24H2.026C.9 24 0 23.1 0 21.976V2.026C0 .9.9 0 2.026 0h19.95C23.1 0 24 .9 24 2.026v19.95C24 23.1 23.1 24 21.976 24zm-3.953-8.353c0-1.18-.966-2.135-2.15-2.135h-.592c-.363 0-.66-.295-.66-.655v-1.714c0-1.18-.966-2.136-2.15-2.136H8.13c-1.184 0-2.15.956-2.15 2.136v5.358c0 1.18.966 2.136 2.15 2.136h9.743c1.184 0 2.15-.956 2.15-2.136v-.854zm0-7.177c0-1.18-.966-2.136-2.15-2.136H8.13c-1.184 0-2.15.956-2.15 2.136v.427c0 1.18.966 2.136 2.15 2.136h7.743c1.184 0 2.15-.956 2.15-2.136v-.427zm-3.103 7.603H9.08a.642.642 0 0 1 0-1.284h5.84a.642.642 0 0 1 0 1.284zm0-2.991H9.08a.642.642 0 0 1 0-1.284h5.84a.642.642 0 0 1 0 1.284zm0-4.275H9.08a.642.642 0 0 1 0-1.284h5.84a.642.642 0 0 1 0 1.284z"/></svg></a></div>`;
